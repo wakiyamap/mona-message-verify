56584372ee3ff25a76cdef79444c6fbad8f26f0a
wakiyamap <wakiyamap@gmail.com>
2020-10-28 23:48:08 +0900

    countermesure of ddos & excluding post

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ http.createServer(function (req, res) {
     req.on('data', function (chunk) {
       data += chunk
     }).on('end', function () {
-      console.log(data);
+      if (data.length > 1000) {
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.write("404 Not Found\n");
+        res.end();
+      }
       try {
         const obj = JSON.parse(data);
         res.writeHead(200, {'Content-Type' : 'text/html'});
@@ -22,5 +26,9 @@ http.createServer(function (req, res) {
         res.end();
       }
     })
+  } else {
+    res.writeHead(404, {"Content-Type": "text/plain"});
+    res.write("404 Not Found\n");
+    res.end();
   }
 }).listen(3000);

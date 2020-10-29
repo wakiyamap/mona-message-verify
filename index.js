76707fef12b259113f17475343c6a7b0c5750264
Jin Eguchi <wakiyamap@gmail.com>
2020-10-29 09:42:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ http.createServer(function (req, res) {
     req.on('data', function (chunk) {
       data += chunk
     }).on('end', function () {
-      if (data.length > 1000) {
+      if (data.length > 3000) {
         res.writeHead(404, {"Content-Type": "text/plain"});
         res.write("404 Not Found\n");
         res.end();
